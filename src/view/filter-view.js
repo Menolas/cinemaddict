5d936dd1c9b23ddb0240bc281f7f2b67cbc566ae
Elena Christensen <olenachristensen777@gmail.com>
2021-12-04 13:30:23 +0100

    linter errors

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,12 +1,10 @@
 import {createElement} from '../render.js';
 
-const createFilterTemplate = () => {
-  return `<ul class="sort">
+const createFilterTemplate = () => `<ul class="sort">
             <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
             <li><a href="#" class="sort__button">Sort by date</a></li>
             <li><a href="#" class="sort__button">Sort by rating</a></li>
           </ul>`;
-};
 
 export default class FilterView {
   #element = null;
--- src/view/site-menu-view.js
+++ src/view/site-menu-view.js
@@ -1,8 +1,7 @@
 import {createElement} from '../render.js';
 import {filteredData} from '../main.js';
 
-const createSiteMenuTemplate = () => {
-  return `<nav class="main-navigation">
+const createSiteMenuTemplate = () => `<nav class="main-navigation">
             <div class="main-navigation__items">
               <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
               <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${filteredData.watchList}</span></a>
@@ -10,9 +9,7 @@ const createSiteMenuTemplate = () => {
               <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${filteredData.favourites}</span></a>
             </div>
             <a href="#stats" class="main-navigation__additional">Stats</a>
-          </nav>
-         `;
-};
+          </nav>`;
 
 export default class SiteMenuView {
   #element = null;
