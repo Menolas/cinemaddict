5ff1f8f17d58e99fbfa97a9a2605ef9d7925131c
Elena Christensen <olenachristensen777@gmail.com>
2021-12-04 17:22:41 +0100

    linter

--- src/view/film-board-view.js
+++ src/view/film-board-view.js
@@ -1,6 +1,7 @@
 import {createElement} from '../render.js';
 
-const createFilmBoardTemplate = () => (`<section class="films"></section>`);
+const createFilmBoardTemplate = () => `<section class="films">
+  </section>`;
 
 export default class FilmBoardView {
   #element = null;
