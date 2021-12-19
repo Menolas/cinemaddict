207be437a3908bd65d5c8e3e942587e63bc746c7
Elena Christensen <olenachristensen777@gmail.com>
2021-12-19 21:12:30 +0100

    linter

--- src/view/film-list-view.js
+++ src/view/film-list-view.js
@@ -1,7 +1,7 @@
 import AbstractView from './abstract-view.js';
 
 const createFilmListTemplate = () => (
-  return `<div class="films-list__container"></div>`;
+  `<div class="films-list__container"></div>`
 );
 
 export default class FilmListView extends AbstractView {
